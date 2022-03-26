324554cb742d1c44d2f7dd99df6571698db29407
ckt22 <tangchekkit@kidpairing.com>
2022-03-26 22:45:54 +0800

    increase initial supply

--- scripts/deploy_token.py
+++ scripts/deploy_token.py
@@ -2,7 +2,7 @@ from scripts.helpful_scripts import get_account_v2
 from brownie import MaryToken
 from web3 import Web3
 
-initial_supply = Web3.toWei(1000, "ether")
+initial_supply = Web3.toWei(100000, "ether")
 
 def main():
     account = get_account_v2()
