53ea566e85fe64e4e273e84167fbc7c5d248ccd0
ckt22 <tangchekkit@kidpairing.com>
2022-03-26 15:13:48 +0800

    Rename MaryCoin to MaryToken

--- contracts/MaryCoin.sol
+++ contracts/MaryCoin.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MaryCoin is ERC20 {
+contract MaryToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Mary", "MARY") {
         _mint(msg.sender, initialSupply);
     }
--- scripts/deploy_token.py
+++ scripts/deploy_token.py
@@ -1,9 +1,9 @@
 from scripts.helpful_scripts import get_account_v2
-from brownie import MaryCoin
+from brownie import MaryToken
 from web3 import Web3
 
 initial_supply = Web3.toWei(1000, "ether")
 
 def main():
     account = get_account_v2()
-    mary_coin = MaryCoin.deploy(initial_supply, {"from": account}, publish_source=True)
+    mary_token = MaryToken.deploy(initial_supply, {"from": account}, publish_source=True)
